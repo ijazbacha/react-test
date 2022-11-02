efd38f1cb3a59a8c892bd3073eddf7f1bf3378da
ijaz bacha <ijazbacha14@gmail.com>
2022-11-02 17:46:08 +0500

    Update Button.js

--- src/components/customButton/Button.js
+++ src/components/customButton/Button.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
-function Button({children, onClick, type}) {
+function Button({ children, onClick, type, buttonStyle, buttonSize }) {
+  const STYLE = [
+    "btn--primary--solid",
+    "btn--warning--solid",
+    "btn--danger--solid",
+    "btn--success--solid",
+    "btn--primary--outline",
+    "btn--warning--outline",
+    "btn--danger--outline",
+    "btn--success--outline",
+  ];
+
   return (
     <button onClick={onClick} type={type}>
       {children}
     </button>
-  )
+  );
 }
 
-export default Button
+export default Button;
