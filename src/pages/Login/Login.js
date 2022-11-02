ff4bb19350f7c334c6034b11f8d67b795892bfeb
ijaz bacha <ijazbacha14@gmail.com>
2022-11-02 17:45:17 +0500

    create btn component

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Button from '../../components/customButton/Button'
 import './Login.css'
 
 function Login() {
+  const loginHandler = () =>{
+    alert(" login button clicked")
+  }
   return (
     <div className='container'>
       <div className='loginContainer'>
-        <h1 className='loginText'>Login</h1>
+        <h1 className='loginText'>Login to Your Account</h1>
+        <Button onClick={loginHandler}>Login</Button>
       </div>
     </div>
   )
