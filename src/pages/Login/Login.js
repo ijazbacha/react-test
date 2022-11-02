4c9be301b3c575d5c7c7a9f26347da61ef94af86
ijaz bacha <ijazbacha14@gmail.com>
2022-11-02 17:22:55 +0500

    create a login page

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+  return (
+    <div className='container'>
+      <div className='loginContainer'>
+        <h1 className='loginText'>Login</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Login
